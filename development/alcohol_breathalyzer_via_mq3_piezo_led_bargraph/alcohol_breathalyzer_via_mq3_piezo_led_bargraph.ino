92883d379e8a58227e03eadc2802a212e98e609b
Efstathios Chatzikyriakidis <stathis.chatzikyriakidis@gmail.com>
2022-04-25 00:24:13 +0300

    Change.

--- development/alcohol_breathalyzer_via_mq3_piezo_led_bargraph/alcohol_breathalyzer_via_mq3_piezo_led_bargraph.ino
+++ development/alcohol_breathalyzer_via_mq3_piezo_led_bargraph/alcohol_breathalyzer_via_mq3_piezo_led_bargraph.ino
@@ -1,7 +1,7 @@
 /*
  *  Alcohol Breathalyzer Via MQ-3, Piezo & Led Bargraph.
  *
- *  Copyright (C) 2010 Efstathios Chatzikyriakidis (contact@efxa.org)
+ *  Copyright (C) 2010 Efstathios Chatzikyriakidis (stathis.chatzikyriakidis@gmail.com)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
